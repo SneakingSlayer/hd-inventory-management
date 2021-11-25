8fde9cc23d8a7c19c55422914eea747c10ebf397
SneakingSlayer <endayalance@yahoo.com>
2021-11-25 20:03:13 +0800

    test

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api', routes);
-app.use('', test)
+app.use('/u', test)
 /**if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
 }*/
--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.get('/', async (req,res) => {
+router.get('/:id', async (req,res) => {
     res.redirect('https://www.youtube.com/watch?v=TtASKYcPfkc&ab_channel=lj40d')
 })
 
